560cfccd14d95961c95d4c50becb1cf99d86bc37
AtilaGuler <108232136+AtiGuler@users.noreply.github.com>
2023-05-15 00:41:37 +0300

    google auth added

--- src/Components/SignInForm.js
+++ src/Components/SignInForm.js
@@ -1,12 +1,24 @@
 import {useState} from 'react';
-import {Form, Button, Container} from 'react-bootstrap';
+import {Form, Button, Container, Col, Row, Image} from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 function SignInForm() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async function (e) {
+  const handleClickGoogle = async function (e) {
+    try {
+      //console.log(`${process.env.REACT_APP_API}/auth/google`);
+      //const res = await axios.get(`${process.env.REACT_APP_API}/auth/google`);
+      window.open(`${process.env.REACT_APP_API}/auth/google`, '_self');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClickSubmit = async function (e) {
     try {
       e.preventDefault();
     } catch (error) {
@@ -16,7 +28,7 @@ function SignInForm() {
 
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
+      className="d-flex flex-column align-items-center justify-content-center"
       style={{minHeight: '80vh'}}
     >
       <Form style={{width: '100%', maxWidth: '400px'}}>
@@ -47,9 +59,31 @@ function SignInForm() {
             className="text-light"
           />
         </Form.Group>
-        <Button onClick={handleSubmit} variant="primary" type="submit">
-          Sign In
-        </Button>
+        <Col className="d-grid gap-2">
+          <Button onClick={handleClickSubmit} variant="primary" type="submit">
+            Sign In
+          </Button>
+          <Row className="my-3 align-items-center">
+            <Col xs={5}>
+              <hr className="m-0" />
+            </Col>
+            <Col xs={2} className="text-center">
+              or
+            </Col>
+            <Col xs={5}>
+              <hr className="m-0" />
+            </Col>
+          </Row>
+
+          <Button variant="outline-light" onClick={handleClickGoogle}>
+            <Image
+              className="me-2"
+              src="https://img.icons8.com/color/16/000000/google-logo.png"
+              alt="Google Icon"
+            />
+            Google
+          </Button>
+        </Col>
       </Form>
     </Container>
   );
